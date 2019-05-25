7684491b411087a1df0813e7851f72140e16a5a0
Jasey Weedseed <kingedgar@gmail.com>
2019-05-25 13:33:27 -0500

    change /config to /conf

--- root/etc/cont-init.d/91_configure_darkhttpd.sh
+++ root/etc/cont-init.d/91_configure_darkhttpd.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/with-contenv bash
 
-dockerize -template /app/start_darkhttpd.sh:/config/start_darkhttpd.sh
+dockerize -template /app/start_darkhttpd.sh:/conf/start_darkhttpd.sh
 
-chmod +x /config/start_darkhttpd.sh
+chmod +x /conf/start_darkhttpd.sh
 
-chown -R app:users /config
+chown -R app:users /conf
